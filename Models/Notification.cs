5b5635d2abbd066341800679bd280b1790a8f114
PATRICIOBURBANO <patricioburbano@gmail.com>
2022-03-31 21:16:55 -0500

    Add project files.

---
+++ Models/Notification.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectManagement.Models
+{
+    public class Notification
+    {
+
+        int Id { get; set; }
+
+        bool Status { get; set; }
+
+    }
+}
---
+++ Models/Project.cs
@@ -0,0 +1,25 @@
+﻿namespace ProjectManagement.Models
+{
+    public class Project
+    {
+
+        int Id { get; set; } 
+
+        string Name { get; set; }
+
+        int CompletedPercentage { get; set; }   
+        
+        float Budget { get; set; }  
+        
+        ICollection<Task> Tasks { get; set; }   
+
+        public Project ()
+        {
+
+            Tasks = new List<Task> ();  
+
+        }
+
+
+    }
+}
