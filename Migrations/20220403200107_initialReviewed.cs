ae6859080ca2bbee3b00e32c5a8699f638cb4447
PATRICIOBURBANO <patricioburbano@gmail.com>
2022-04-03 21:00:46 -0500

    Add Seed Data

---
+++ Migrations/20220403200107_initialReviewed.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectManagement.Migrations
+{
+    public partial class initialReviewed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
